71e9b72d686350c15e8b8a0fc7c4cb159c9e25f6
Danny Fuhriman <eatplaysleep@gmail.com>
2022-10-28 16:59:13 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
-echo "===========================================================================
-      |            Running challenge setup script on host container             |
-      ==========================================================================="
-echo "===========================================================================
-      |                     Setting custom VSCode settings                      |
-      |-------------------------------------------------------------------------|
-      |                                                                         |
-      | `mkdir -p ~/user-data/User`                                             |
-      |                                                                         |
-      | `cat > ~/user-data/User/settings.json <<EOF                             |
-      | {                                                                       |
-      |     "explorer.fileNesting.enabled": true,                               |
-      |     "editor.formatOnSave": true,                                        |
-      |     "editor.formatOnPaste": true,                                       |
-      |     "files.autoSave": "off",                                            |
-      |     "security.workspace.trust.banner": "never",                         |
-      |     "security.workspace.trust.enabled": false,                          |
-      |     "security.workspace.trust.startupPrompt": "never",                  |
-      |     "security.workspace.trust.untrustedFiles": "open",                  |
-      |     "terminal.integrated.defaultProfile.linux": "bash",                 |
-      |     "workbench.colorTheme": "Default Dark+",                            |
-      |     "workbench.editor.highlightModifiedTabs": true,                     |
-      |     "workbench.startupEditor": "readme"                                 |
-      | }                                                                       |
-      | EOF`                                                                    |
-      ==========================================================================="
+echo "Running challenge setup script on host container"
+echo "===> Setting custom VSCode settings..."
 mkdir -p ~/user-data/User
 cat > ~/user-data/User/settings.json <<EOF
 {
@@ -43,35 +19,15 @@ cat > ~/user-data/User/settings.json <<EOF
 }
 EOF
 
-echo "===========================================================================
-      |                         Installing NodeJS                               |
-      |-------------------------------------------------------------------------|
-      |                                                                         |
-      | `curl -fsSL https://deb.nodesource.com/setup_16.x | bash -1`            |
-      |                                                                         |
-      | `apt-get install -y nodejs`                                             |
-      ==========================================================================="
+echo "===>Installing NodeJS"
 # https://github.com/nodesource/distributions/blob/master/README.md
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
 apt-get install -y nodejs
 
-echo "===========================================================================
-      |                       Cloning the template repo                         |
-      |-------------------------------------------------------------------------|
-      |                                                                         |
-      | `git clone https://github.com/udplabs/auth-rocks-app-template.git ~/app`|
-      |                                                                         |
-      | `apt-get install -y nodejs`                                             |
-      ==========================================================================="
+echo "===>Cloning template repo..."
 git clone https://github.com/udplabs/auth-rocks-app-template.git ~/app
 
-echo "===========================================================================
-      |                                 Changing CWD                            |
-      |-------------------------------------------------------------------------|
-      |                                                                         |
-      | `cd ~/app`                                                              |
-      |                                                                         |
-      | `npm ci`                                                                |
-      ==========================================================================="
+echo "===>Changing CWD..."
+echo "===>Installing dependencies..."
 cd ~/app
 npm ci
