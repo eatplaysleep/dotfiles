584743b4b3020e5cf3f3cbb12a3740fd038b0a43
Danny Fuhriman <eatplaysleep@gmail.com>
2022-10-28 15:29:45 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,22 +2,32 @@
 echo "Running challenge setup script on host container"
 
 # Adjust VSCode settings
-mkdir -p /user-data/User
-cat > /user-data/User/settings.json <<EOF
+mkdir -p ~/user-data/User
+cat > ~/user-data/User/settings.json <<EOF
 {
-  "workbench.colorTheme": "Default Dark+",
-  "security.workspace.trust.enabled": false
+    "explorer.fileNesting.enabled": true,
+    "editor.formatOnSave": true,
+    "editor.formatOnPaste": true,
+    "files.autoSave": "off",
+    "security.workspace.trust.banner": "never",
+    "security.workspace.trust.enabled": false,
+    "security.workspace.trust.startupPrompt": "never",
+    "security.workspace.trust.untrustedFiles": "open",
+    "terminal.integrated.defaultProfile.linux": "bash",
+    "workbench.colorTheme": "Default Dark+",
+    "workbench.editor.highlightModifiedTabs": true,
+	"workbench.startupEditor": "readme"
 }
 EOF
 
 # Install Node.js
 # https://github.com/nodesource/distributions/blob/master/README.md
-curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
-apt-get install -y nodejs
+# curl -fsSL https://deb.nodesource.com/setup_14.x | bash -
+# apt-get install -y nodejs
 
 # Clone the example repository
-git clone https://github.com/udplabs/auth-rocks-app-template.git /app
+git clone https://github.com/udplabs/auth-rocks-app-template.git ~/app
 
 # Change directory to the cloned repository and install its dependencies
-cd /app
+cd ~/app
 npm ci
