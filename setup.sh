e570949ab6e9dc1e6982e2562d43a88c72f0c06b
Danny Fuhriman <eatplaysleep@gmail.com>
2022-11-01 16:30:55 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,33 +1,113 @@
 #!/bin/bash
-echo "Running challenge setup script on host container"
-echo "===> Setting custom VSCode settings..."
-mkdir -p /home/coder/user-data/User
-cat > /home/coder/user-data/User/settings.json <<EOF
-{
-    "explorer.fileNesting.enabled": true,
-    "editor.formatOnSave": true,
-    "editor.formatOnPaste": true,
-    "files.autoSave": "off",
-    "security.workspace.trust.banner": "never",
-    "security.workspace.trust.enabled": false,
-    "security.workspace.trust.startupPrompt": "never",
-    "security.workspace.trust.untrustedFiles": "open",
-    "terminal.integrated.defaultProfile.linux": "bash",
-    "workbench.colorTheme": "Default Dark+",
-    "workbench.editor.highlightModifiedTabs": true,
-    "workbench.startupEditor": "readme"
-}
-EOF
 
-echo "===>Installing NodeJS"
-# https://github.com/nodesource/distributions/blob/master/README.md
-curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
-apt-get install -y nodejs
+set -euxo pipefail
 
-echo "===>Cloning template repo..."
-git clone https://github.com/udplabs/auth-rocks-app-template.git /home/coder/app
+echo "Running track setup script"
 
-echo "===>Changing CWD..."
-echo "===>Installing dependencies..."
-cd /home/coder/app
+# -----------------------------------
+## We export a dollar sign in order to utilize variables in our NGINX config.
+## See https://www.baeldung.com/linux/nginx-config-environment-variables for details.
+export DOLLAR="$"
+# -----------------------------------
+
+# ------------ VARIABLES ------------
+export APP_PATH='/root/.local/share/code-server/app'
+export TEMPLATES=$APP_PATH'/.vscode'
+CERT_CONTACT='danny.fuhriman@okta.com'
+CODE_SERVER_ENV_TEMPLATE=$TEMPLATES'/code-server.env.template'
+CODE_SERVER_ENV='/etc/code-server'
+# CODE_SERVER_INSTALL_URL='https://code-server.dev/install.sh'
+NGINX_CONFIG_FILE='/etc/nginx/sites-available/app-server.conf'
+NGINX_CONFIG_LINK='/etc/nginx/sites-enabled/app-server.conf'
+NGINX_CONFIG_TEMPLATE=$TEMPLATES'/app-server.conf.template'
+NODEJS_SRC_URL='https://deb.nodesource.com/setup_16.x'
+export SANDBOX_URL=$HOSTNAME.$_SANDBOX_ID.instruqt.io
+START_SCRIPT_TEMPLATE=$TEMPLATES'/code-server.service.template'
+START_SCRIPT_FILE='/etc/systemd/system/code-server.service'
+export USER_SETTINGS_DIR='/root/.local/share/code-server/.vscode'
+USER_SETTINGS=$USER_SETTINGS_DIR'/User'
+# -----------------------------------
+
+
+# ------ Refresh Package Index ------
+# apt-get -y update
+# -----------------------------------
+
+
+# ------- Install Code Server -------
+# curl -fsSL $CODE_SERVER_INSTALL_URL | sh
+# -----------------------------------
+
+
+# --------- Install NodeJS ----------
+## See https://github.com/nodesource/distributions/blob/master/README.md
+curl -fsSL $NODEJS_SRC_URL | bash -
+# apt-get install -y nodejs
+# -----------------------------------
+
+
+# --------- Setup Workspace ---------
+## Creat directories for user data & code
+mkdir -p $APP_PATH
+mkdir -p $USER_SETTINGS
+mkdir -p $CODE_SERVER_ENV
+
+## Clone the example repository
+git clone https://github.com/udplabs/auth-rocks-app-template.git $APP_PATH
+
+## Change directory and install dependencies
+cd $APP_PATH
 npm ci
+
+## Create VS Code user settings
+cp $APP_PATH/.vscode/settings.json $USER_SETTINGS
+# -----------------------------------
+
+
+# ----------- Setup NGINX -----------
+## For details on why we are doing this, see https://coder.com/docs/code-server/latest/guide#using-lets-encrypt-with-nginx
+
+## Install nginx
+apt-get install -y nginx
+
+## Install/Update snapd
+snap install core
+snap refresh core
+
+## Install certbot
+snap install --classic certbot
+ln -s /snap/bin/certbot /usr/bin/certbot
+
+## Install & Configure Route53 plugin
+snap set certbot trust-plugin-with-root=ok
+snap install certbot-dns-route53
+
+### Add AWS credentials to service env file
+envsubst < $CODE_SERVER_ENV_TEMPLATE > $CODE_SERVER_ENV'/env'
+
+# apt-get install -y nginx certbot python3-certbot-nginx
+
+## Generate && set nginx configuration
+envsubst < $NGINX_CONFIG_TEMPLATE > $NGINX_CONFIG_FILE
+
+## Enable config and generate cert
+ln -s $NGINX_CONFIG_FILE $NGINX_CONFIG_LINK
+certbot --non-interactive --redirect --agree-tos -d $SANDBOX_URL -m $CERT_CONTACT --nginx
+# certbot certonly --cert-name $SANDBOX_URL --dns-route53 -d gentle-animal.auth.rocks
+
+if pgrep -x nginx >/dev/null; then
+    echo "The nginx service is running. Restart it!"
+    
+    ## Reload nginx
+    nginx -s reload
+fi
+# -----------------------------------
+
+# ----- Generate Startup Script -----
+envsubst < $START_SCRIPT_TEMPLATE > $START_SCRIPT_FILE
+# -----------------------------------
+
+
+# Start Code Server
+# systemctl enable code-server
+# systemctl start code-server
