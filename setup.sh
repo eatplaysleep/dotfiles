bfa1a6bf031a72bfb171f8e9f336be7db26e900e
Danny Fuhriman <eatplaysleep@gmail.com>
2022-11-01 16:56:45 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,26 +28,25 @@ export USER_SETTINGS_DIR='/root/.local/share/code-server/.vscode'
 USER_SETTINGS=$USER_SETTINGS_DIR'/User'
 # -----------------------------------
 
-
-# ------ Refresh Package Index ------
-# apt-get -y update
-# -----------------------------------
-
-
 # ------- Install Code Server -------
 # curl -fsSL $CODE_SERVER_INSTALL_URL | sh
 # -----------------------------------
 
+# ------ Refresh Package Index ------
+apt-get -y update
+# -----------------------------------
 
 # --------- Install NodeJS ----------
 ## See https://github.com/nodesource/distributions/blob/master/README.md
-curl -fsSL $NODEJS_SRC_URL | bash -
-# apt-get install -y nodejs
+if ![[ -x /usr/local/bin/node ]]; then
+    curl -fsSL $NODEJS_SRC_URL | bash -
+    apt-get install -y nodejs
+fi
 # -----------------------------------
 
 
 # --------- Setup Workspace ---------
-## Creat directories for user data & code
+## Create directories for user data & code
 mkdir -p $APP_PATH
 mkdir -p $USER_SETTINGS
 mkdir -p $CODE_SERVER_ENV
@@ -67,39 +66,50 @@ cp $APP_PATH/.vscode/settings.json $USER_SETTINGS
 # ----------- Setup NGINX -----------
 ## For details on why we are doing this, see https://coder.com/docs/code-server/latest/guide#using-lets-encrypt-with-nginx
 
-## Install nginx
-apt-get install -y nginx
+## Install nginx (if not present)
+if ! [[ -x /usr/local/bin/nginx ]]; then
+    apt-get install -y nginx
+fi
 
-## Install/Update snapd
-snap install core
+## Install snapd (if not present)
+if ! [[ -x /usr/local/bin/snap ]]; then
+    snap install core
+fi
+## Refresh snapd core
 snap refresh core
 
-## Install certbot
-snap install --classic certbot
-ln -s /snap/bin/certbot /usr/bin/certbot
+## Install certbot if not present
+if ! [[ -x /usr/local/bin/certbot ]]; then
+    snap install --classic certbot
+    ln -s /snap/bin/certbot /usr/bin/certbot
+fi
 
 ## Install & Configure Route53 plugin
 snap set certbot trust-plugin-with-root=ok
 snap install certbot-dns-route53
 
+## Install envsubst (if not installed)
+if ! [[ -x /usr/local/bin/envsubst ]]; then
+    apt-get install gettext-base
+fi
+
 ### Add AWS credentials to service env file
 envsubst < $CODE_SERVER_ENV_TEMPLATE > $CODE_SERVER_ENV'/env'
-
-# apt-get install -y nginx certbot python3-certbot-nginx
 
 ## Generate && set nginx configuration
 envsubst < $NGINX_CONFIG_TEMPLATE > $NGINX_CONFIG_FILE
 
 ## Enable config and generate cert
 ln -s $NGINX_CONFIG_FILE $NGINX_CONFIG_LINK
-certbot --non-interactive --redirect --agree-tos -d $SANDBOX_URL -d gentle-animal.auth.rocks -m $CERT_CONTACT --nginx
+certbot run --non-interactive --redirect --agree-tos -a dns-route53 -i nginx -d $SANDBOX_URL -d gentle-animal.auth.rocks -m $CERT_CONTACT --allow-subset-of-names
 # certbot certonly --cert-name $SANDBOX_URL --dns-route53 -d gentle-animal.auth.rocks
 
 if pgrep -x nginx >/dev/null; then
     echo "The nginx service is running. Restart it!"
     
     ## Reload nginx
-    nginx -s reload
+#     nginx -s reload
+    service nginx reload
 fi
 # -----------------------------------
 
